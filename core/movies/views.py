c454eed2101b0ad18d47e166de003f926f808e0e
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-26 03:48:25 +0000

    tag filtering with alpine

--- core/movies/views.py
+++ core/movies/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .serializers import MovieSerializer
 from rest_framework.generics import ListAPIView
 from .models import Movie
+from taggit.models import Tag
+
+
+def index(request):
+    movies = Movie.objects.prefetch_related("tags").all()
+    tags = Tag.objects.all()
+    context = {"movies": movies, "tags": tags}
+    return render(request, "core/index.html", context)
 
 
 class MovieListAPIView(ListAPIView):
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.movies import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("movies/", include("core.movies.urls", namespace="movies")),
+    path("", views.index, name="index"),
 ]
